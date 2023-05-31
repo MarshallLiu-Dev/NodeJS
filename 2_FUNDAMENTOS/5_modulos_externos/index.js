f248f56d960c1c6cbfa7479b3c1241615052f1a6
Paulo Henrique M de Morais <111690624+MarshallLiu-Dev@users.noreply.github.com>
2023-05-31 10:38:39 -0300

    frist commit

--- 2_FUNDAMENTOS/5_modulos_externos/index.js
+++ 2_FUNDAMENTOS/5_modulos_externos/index.js
@@ -4,4 +4,8 @@ const args = minimist(process.argv.slice(2));
 console.log(args);
 
 const nome = args['nome']
-console.log(nome);
+const profissao = args['profissao']
+const nick = args['nick']
+console.log(nome, nick, profissao);
+
+console.log(`O nome dele é ${nome} e ele é ${profissao} e seu NickName é ${nick}`);
